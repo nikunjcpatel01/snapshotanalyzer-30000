2586730c46c90dde62b2dd044e09073b250dd380
NikShama <nikshama@NikShamas-MacBook-Pro.local>
2019-08-10 23:06:10 +0800

    Added snapshot functionality

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -78,16 +78,26 @@ def instances():
 
 @instances.command('snapshot',help="Create snapshots for all volumes")
 @click.option('--project',default=None,help="Only instances for project(tag Project:<name>)")
-def create_snapshots():
+
+def create_snapshots(project):
     "Create snapshots for EC2 instances"
 
     instances=filter_instances(project)
 
     for i in instances:
+        print("Stopping {0}...".format(i.id))
         i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
             print("Creating snapshot of {0}".format(v.id))
-            v.create_snapshots(Description="Created by snapshotAnalyzer 30000")
+            v.create_snapshot(Description="Created by snapshotAnalyzer 30000")
+
+        print("Starting {0}...".format(i.id))
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done")
 
     return
 
